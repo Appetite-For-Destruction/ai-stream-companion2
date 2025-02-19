c556f3f4601f398c71c7983c89b9e3b5d5e5354a
sumino <suminoshuto@gmail.com>
2025-02-19 15:27:11 +0900

    Initial commit

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,50 +1,14 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
-const config = {
-  darkMode: ["class"],
+const config: Config = {
+  // Tailwindの設定をここに追加
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}', // 対象ファイル
   ],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    extend: {
-      keyframes: {
-        "slide-left": {
-          "0%": { 
-            transform: "translateX(100%)",
-            opacity: "0"
-          },
-          "100%": { 
-            transform: "translateX(0)",
-            opacity: "1"
-          }
-        },
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "slide-left": "slide-left 0.5s ease-out forwards",
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
-    },
+    extend: {},
   },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+  plugins: [],
+};
 
 export default config;
